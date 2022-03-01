1d92fce8089463c37d3d5d9b41eb3b42474a64b5
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-03-01 19:42:20 +0300

    Add functionality to close the menu when the ecit button and the links are clicked

--- menu.js
+++ menu.js
@@ -2,16 +2,19 @@
 var menuIcon = document.querySelector('#menu');
 var mobileMenu = document.querySelector('#mobile-menu');
 var closeMenu = document.querySelector('#menu-exit');
-var menuLinks = document.querySelectorAll('#menu-link');
+var menuLinks = document.querySelectorAll('.menu-link');
 
 
 menuIcon.addEventListener('click', displayMenu);
 closeMenu.addEventListener('click', exitMenu);
 menuLinks.forEach(link => {
-  link.addEventListener('click', exitMenu)
+  link.addEventListener('click', exitMenu);
 })
 
-function displayMenu(e) {
-  e.preventDefault();
+function displayMenu() {
   mobileMenu.style.transform = 'translateX(0%)';
+}
+
+function exitMenu() {
+  mobileMenu.style.transform = 'translateX(-100%)';
 }
