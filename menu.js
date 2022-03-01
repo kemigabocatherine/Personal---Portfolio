17df996060c0500684eb8b6cf921ddb0d45ce53f
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-03-01 23:04:20 +0300

    FIx linter errors

--- menu.js
+++ menu.js
@@ -1,15 +1,8 @@
-/*Mobile-menu*/
-var menuIcon = document.querySelector('#menu');
-var mobileMenu = document.querySelector('#mobile-menu');
-var closeMenu = document.querySelector('#menu-exit');
-var menuLinks = document.querySelectorAll('.menu-link');
-
-
-menuIcon.addEventListener('click', displayMenu);
-closeMenu.addEventListener('click', exitMenu);
-menuLinks.forEach(link => {
-  link.addEventListener('click', exitMenu);
-})
+/* Mobile-menu */
+const menuIcon = document.querySelector('#menu');
+const mobileMenu = document.querySelector('#mobile-menu');
+const closeMenu = document.querySelector('#menu-exit');
+const menuLinks = document.querySelectorAll('.menu-link');
 
 function displayMenu() {
   mobileMenu.style.transform = 'translateX(0%)';
@@ -18,3 +11,9 @@ function displayMenu() {
 function exitMenu() {
   mobileMenu.style.transform = 'translateX(-100%)';
 }
+
+menuIcon.addEventListener('click', displayMenu);
+closeMenu.addEventListener('click', exitMenu);
+menuLinks.forEach(link => {
+  link.addEventListener('click', exitMenu);
+})
