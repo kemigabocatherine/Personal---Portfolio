8a022b5439d31c0cadc3de64e4d01af7dacc4398
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-03-03 17:18:41 +0300

    Add functionality to popup window

--- menu.js
+++ menu.js
@@ -18,44 +18,74 @@ menuLinks.forEach((link) => {
   link.addEventListener('click', exitMenu);
 });
 
-// PROJECT SECTION & POP UP WINDOW
-const cardContent = {
-  images: {
-    card1_mobile:'images2/SnapshootPortfolio.png',
-    card2_mobile:'images2/SnapshootPortfolio(1).png',
-    card3_mobile:'images2/SnapshootPortfolio(2).png',
-    card4_mobile:'images2/SnapshootPortfolio(3).png',
-    card1_desktop:'images3/Desktopcard1.png',
-    card2_desktop:'images3/desktop-project2.png',
-    card3_desktop:'images3/desktop-project3.png',
-    card4_desktop:'images3/desktop-project4.png',
-    popup1:'images/icon.png',
-    popup2:'images/popup.png'
+//POPUP WINDOW
+const projects = [
+  {
+    /* Has id=0 in HTML because this is the first element in this array with index 0 */
+    name: 'Tonic',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    image: 'images2/SnapshootPortfolio.png',
+    imageGit: 'images/popup.png',
+    imageLive: 'images/Icon.png',
+
   },
+  {
+    /* Has id=1 in HTML because this is the first element in this array with index 1 */
+    name: 'Multi-Post Stories',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    image: 'images2/SnapshootPortfolio(1).png',
+    imageGit: 'images/popup.png',
+    imageLive: 'images/Icon.png',
 
-  headings: {
-    card1_3:'Tonic',
-    card2_4:'Multi-Post Stories',
-    card3_desktop:'Facebook 360',
-    card4_desktop:'Uber Navigation'
   },
+  {
+    /* Has id=2 in HTML because this is the first element in this array with index 2 */
+    name: 'Tonic',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    image: 'images2/SnapshootPortfolio(2).png',
+    imageGit: 'images/popup.png',
+    imageLive: 'images/Icon.png',
 
-  paragraph: {
-    cards:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    popup:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
   },
+  {
+    /* Has id=3 in HTML because this is the first element in this array with index 3 */
+    name: 'Multi-Post Stories',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    image: 'images2/SnapshootPortfolio(3).png',
+    imageGit: 'images/popup.png',
+    imageLive: 'images/Icon.png',
 
-  list: {
-    links: [
-      'CANOPY', 'Back End Dev', '2015'
-    ],
-
-    technologies: [
-      'html', 'css', 'javascript'
-    ]
   },
+];
 
-  buttons: [
-    'See project', 'See live', 'See Source'
-  ]
-}
+const seenProject = document.querySelectorAll('.pro1');
+const main = document.querySelector('.main');
+seenProject.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonId = button.getAttribute('id');
+    const projectDetails = document.querySelector('.project_details');
+    projectDetails.classList.add('showpopup');
+    main.classList.add('blur');
+    const heading = projectDetails.querySelector('.detail-heading');
+    const paragrah = projectDetails.querySelector('.detail');
+    const image = projectDetails.querySelector('.detail-image');
+    const githubImage = projectDetails.querySelector('.github');
+    const liveImage = projectDetails.querySelector('.liveimage');
+
+    heading.innerHTML = projects[buttonId].name;
+    paragrah.innerHTML = projects[buttonId].description;
+    image.src = projects[buttonId].image;
+    githubImage.src = projects[buttonId].imageGit;
+    liveImage.src = projects[buttonId].imageLive;
+
+    const closePopup = projectDetails.querySelector('.close-popup');
+
+    closePopup.addEventListener('click', () => {
+      projectDetails.classList.remove('showpopup');
+      main.classList.remove('blur');
+    });  
+  })
+  
+})
+
+
