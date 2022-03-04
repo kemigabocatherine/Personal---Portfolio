09fea0dac0b438cd90ea77157cf0a031302fa557
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-03-05 00:21:34 +0300

    Add the popup window using javascript

--- menu.js
+++ menu.js
@@ -58,30 +58,72 @@ const projects = [
   },
 ];
 
-const seenProject = document.querySelectorAll('.pro1');
-const main = document.querySelector('.main');
-seenProject.forEach((button) => {
-  button.addEventListener('click', () => {
-    const buttonId = button.getAttribute('id');
-    const projectDetails = document.querySelector('.project_details');
-    projectDetails.classList.add('showpopup');
-    main.classList.add('blur');
-    const heading = projectDetails.querySelector('.detail-heading');
-    const paragrah = projectDetails.querySelector('.detail');
-    const image = projectDetails.querySelector('.detail-image');
-    const githubImage = projectDetails.querySelector('.sourceimage');
-    const liveImage = projectDetails.querySelector('.liveimage');
+function showModal(){
+  const projectDetails = document.querySelector('.project_details');
+  const seenProject = document.querySelectorAll('.pro1');
+  const main = document.querySelector('.main');
+  seenProject.forEach((button) => {
+    button.addEventListener('click', () => {
+      const buttonId = button.getAttribute('id');
+      projectDetails.classList.add('showpopup');
+      main.classList.add('blur');
 
-    heading.innerHTML = projects[buttonId].name;
-    paragrah.innerHTML = projects[buttonId].description;
-    image.src = projects[buttonId].image;
-    githubImage.src = projects[buttonId].imageGit;
-    liveImage.src = projects[buttonId].imageLive;
-
-    const closePopup = projectDetails.querySelector('[data-close-button]');
-    closePopup.addEventListener('click', () => {
+      const closePopup = projectDetails.querySelector('[data-close-button]');
+      closePopup.addEventListener('click', () => {
       projectDetails.classList.remove('showpopup');
       main.classList.remove('blur');
-    });
-  });
-});
+      })
+    })
+  })
+  const detailContent =projects.map((project) => { 
+    `<div>
+        <h1 class="detail-heading">${project[buttonId].name}</h1>
+        <a id="popup-close" data-close-button class="close-button-project" >&times;</a>
+      </div>
+      <div>
+        <ul class="list">
+          <li class="canopy">CANOPY</li>
+          <li class="end1">Back End Dev</li>
+          <li class="end">2015</li>
+        </ul> 
+      </div>
+      <img class="detail-image" src="${project[buttonId].image}" alt="Snapshoot">
+      <div class="description">
+        <div>
+          <p class="detail">
+            ${project[buttonId].description}
+          </p>
+        </div>
+        <div id="buttons">
+          <div>
+            <ul class="butto multi" id="technologies">
+              <li>
+                <button type="button" class="ht">html</button>
+              </li>
+              <li>
+                <button type="button" class="ssc">css</button>
+              </li>
+              <li>
+                <button type="button" class="sj">javaScript</button>
+              </li>
+            </ul>
+          </div>
+          <button type="button" class=" live">
+            See live  
+            <img src="${project[buttonId].imageLive}" class="liveimage" alt="live-display">
+          </button>
+          <button type="button" class="Source">
+            See Source
+            <img src="${project[buttonId].imageGit}" class="sourceimage" alt="Source">
+          </button>     
+        </div>    
+      </div>
+    `
+  })
+.join('');
+projectDetails.innerHTML = detailContent; 
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  showModal();
+}); 
